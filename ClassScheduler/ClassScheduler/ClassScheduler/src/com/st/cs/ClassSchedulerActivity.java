9a9896ea15ec09cccae1ccf4f82dad41f8cbf371
tkoushik1 <tkoushik1@yahoo.com>
2013-11-19 17:08:02 -0600

    Class Scheduler

---
+++ ClassScheduler/ClassScheduler/ClassScheduler/src/com/st/cs/ClassSchedulerActivity.java
@@ -0,0 +1,18 @@
+package com.st.cs;
+
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+public class ClassSchedulerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        DBHelper dbhelper = new DBHelper(this);
+        SQLiteDatabase db = dbhelper.getWritableDatabase();
+        db.close();
+        
+    }
+}
